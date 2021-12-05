38cfb957afa943fe67d0cb6a138aecf8c92d36cf
xenotrax <beat.gerber@festin.net>
2021-12-05 17:01:57 +0100

    add sounds

--- Assets/SoundPointManager.cs
+++ Assets/SoundPointManager.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class SoundPointManager : MonoBehaviour
 {
@@ -8,9 +11,20 @@ public class SoundPointManager : MonoBehaviour
     public Transform allSoundEmitters;
     public GameObject soundPointPrefab;
 
+    
+    public static List<T> GetRandomElements<T>(IEnumerable<T> list, int elementsCount)
+    {
+        return list.OrderBy(arg => Guid.NewGuid()).Take(elementsCount).ToList();
+    }
     // Start is called before the first frame update
     void Start()
     {
+        //List<Transform> allSoundList = allSoundEmitters.GetComponents<Transform>().ToList();
+        //List<Transform> randomEmitter = GetRandomElements(allSoundList, allSoundEmitters.childCount);
+        //for (int i = 0; i < randomEmitter.Count; i = +2)
+        //{
+        //    randomEmitter[i]
+        //}
         foreach (Transform soundEmitter in allSoundEmitters)
         {
             Debug.Log("ffff-" + soundEmitter.gameObject.name);
